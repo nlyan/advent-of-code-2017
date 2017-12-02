a91a3ce79b4605ef73d6319322551cfaed983c39
Andrew Nelless <andrew@nelless.net>
2017-12-02 19:38:20 +0000

    Day 2 part 1

---
+++ day2/day2.nim
@@ -0,0 +1,34 @@
+import streams
+import strutils
+import sequtils
+
+type
+  Row = seq[int]
+  Spreadsheet = seq[Row]
+
+
+proc readSpreadsheetFile (path: string): Spreadsheet =
+  var fs = newFileStream(path, fmRead)
+  var spreadsheet: Spreadsheet = @[]
+  var line = ""
+
+  while fs.readLine(line):
+    var row: Row = line.splitWhitespace().map(parseInt)
+    if row.len != 0:
+      spreadsheet.add(row)
+
+  return spreadsheet
+
+
+proc calculateChecksum (spreadsheet: Spreadsheet): int =
+  result = 0
+  for row in spreadsheet:
+    var mini = row.foldl(min(a,b))
+    var maxi = row.foldl(max(a,b))
+    result += maxi - mini
+  return result
+
+var spreadsheet = readSpreadsheetFile("input.txt")
+var checksum = calculateChecksum(spreadsheet)
+
+echo checksum
